552939344eb6a7b71580156ca4a03f6837116728
mrboombastik33 <timchik.oleksandr@gmail.com>
2025-09-20 18:39:24 +0300

    schema update

--- server/rfid.sql
+++ server/rfid.sql
@@ -1,36 +1,53 @@
 DROP DATABASE IF EXISTS `rfid_project`;
-
 CREATE DATABASE `rfid_project`;
 USE `rfid_project`;
 
 CREATE TABLE `access_inf` (
-`person_id` INT NOT NULL,
-`person_name` CHAR(40) NOT NULL UNIQUE,
-`phone` CHAR(19) UNIQUE,
-`role_of` BOOLEAN,
-`uid` CHAR(11) UNIQUE
+    `person_id` INT AUTO_INCREMENT PRIMARY KEY,
+    `name` CHAR(40) UNIQUE NOT NULL,
+    `user_id` CHAR(11) UNIQUE
 );
 
 CREATE TABLE `key_inf` (
-`esp_id` INT PRIMARY KEY NOT NULL, 
-`audience` CHAR(30) , 
-`uid` CHAR(11),
-FOREIGN KEY (uid) REFERENCES access_inf(uid)
-ON DELETE SET NULL
-ON UPDATE CASCADE
+    `esp_id` INT PRIMARY KEY NOT NULL,
+    `audience` CHAR(30) NOT NULL,
+    `uid` CHAR(11),
+    FOREIGN KEY (uid) REFERENCES access_inf(user_id)
+        ON DELETE SET NULL
+        ON UPDATE CASCADE
 );
 
 CREATE TABLE `bookings` (
     `id` INT AUTO_INCREMENT PRIMARY KEY,
     `group_name` CHAR(10) NOT NULL,
-    `student_name` CHAR(40) NOT NULL,
-    `audience` CHAR(30) NOT NULL,
+    `user_name` CHAR(11) NOT NULL,
+    `esp_id` INT NOT NULL,
     `booking_time` TIME NOT NULL,
     `day_of_week` CHAR(10) NOT NULL,
-    `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_name) REFERENCES access_inf(name)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    FOREIGN KEY (esp_id) REFERENCES key_inf(esp_id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
 );
 
--- INSERT INTO access_inf(person_id, person_name, phone, role_of, uid)
--- VALUES (1, 'Pavlo', '380507777777', false, '276FE546'), (3, 'admin', '380677777777', true, 'FF');
+
+-- Дані для тестів
+INSERT INTO access_inf (name, user_id)
+VALUES 
+('Pavlo', '276FE546'),
+('admin', 'FF');
+
+INSERT INTO key_inf (esp_id, audience, uid)
+VALUES
+(312, '101A', 'FF'),
+(313, '102B', 'FF'),
+(314, '103C', '276FE546'),
+(315, '807', 'FF');
+
+-- INSERT INTO access_inf(person_id, person_name,uid)
+-- VALUES (1, 'Pavlo', '276FE546'), (3, 'admin', 'FF');
 -- INSERT INTO key_inf (esp_id, audience, uid)
--- VALUES(312, null, 'FF'), (313, null, 'FF'), (314, null, '276FE546');
+-- VALUES(312, 'FF'), (313, 'FF'), (314, '276FE546');
