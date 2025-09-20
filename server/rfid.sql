51623abd44c87eea2dcd9b5ea9ae389570fdeae8
mrboombastik33 <timchik.oleksandr@gmail.com>
2025-09-20 11:07:08 +0300

    table creation

--- server/rfid.sql
+++ server/rfid.sql
@@ -20,7 +20,17 @@ ON DELETE SET NULL
 ON UPDATE CASCADE
 );
 
-INSERT INTO access_inf(person_id, person_name, phone, role_of, uid)
-VALUES (1, 'Pavlo', '380507777777', false, '276FE546'), (3, 'admin', '380677777777', true, 'FF');
-INSERT INTO key_inf (esp_id, audience, uid)
-VALUES(312, null, 'FF'), (313, null, 'FF'), (314, null, '276FE546');
+CREATE TABLE `bookings` (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `group_name` CHAR(10) NOT NULL,
+    `student_name` CHAR(40) NOT NULL,
+    `audience` CHAR(30) NOT NULL,
+    `booking_time` TIME NOT NULL,
+    `day_of_week` CHAR(10) NOT NULL,
+    `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- INSERT INTO access_inf(person_id, person_name, phone, role_of, uid)
+-- VALUES (1, 'Pavlo', '380507777777', false, '276FE546'), (3, 'admin', '380677777777', true, 'FF');
+-- INSERT INTO key_inf (esp_id, audience, uid)
+-- VALUES(312, null, 'FF'), (313, null, 'FF'), (314, null, '276FE546');
